2fd41138ca2a9ccb466f4ddc913ea4124f03c9e9
PlusMinus0 <brandt.matthias+github@gmail.com>
2018-07-27 15:44:24 +0200

    Apply patch in ITK

--- CMakeExternals/ITK.cmake
+++ CMakeExternals/ITK.cmake
@@ -47,9 +47,10 @@ if(NOT DEFINED ITK_DIR)
 
   ExternalProject_Add(${proj}
      LIST_SEPARATOR ${sep}
-     URL "https://github.com/InsightSoftwareConsortium/ITK/archive/v5.0a02.tar.gz"
+    URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/InsightToolkit-4.13.0.tar.xz
+    URL_MD5 3badf70cfb0093054453f66c5974c5a4
      # work with external GDCM
-     # PATCH_COMMAND ${PATCH_COMMAND} -N -p1 -i ${CMAKE_CURRENT_LIST_DIR}/ITK-4.11.0.patch
+     PATCH_COMMAND ${PATCH_COMMAND} -N -p1 -i ${CMAKE_CURRENT_LIST_DIR}/ITK_vcl_compiler.h.patch
      CMAKE_GENERATOR ${gen}
      CMAKE_ARGS
        ${ep_common_args}
